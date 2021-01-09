0433ac68459bfbd4e3f8425e9af6b938e8ad8482
esken <eskindiraleb@yahoo.com>
2021-01-08 19:37:39 -0800

    new proj

---
+++ src/studios/countingcharacters/CharacterCount.java
@@ -0,0 +1,9 @@
+package studios.countingcharacters;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+public class CharacterCount {
+    public static void main(String[] args){
+        char[] charactersInString = myString.toCharArray();
+    }
+}
